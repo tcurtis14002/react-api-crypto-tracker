0958a53e0475724b5ccdb9664ae9518a197cd222
Tyler <willdev0110@gmail.com>
2023-02-12 12:26:58 -0700

    add useEffect for axios, create initial layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => alert("your app STILL does not work"));
+  });
+
   return (
-    <div className="App">
-      <h2>APIII</h2>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form
+          type="text"
+          placeholder="Search"
+          className="coin-input"
+          action=""></form>
+      </div>
     </div>
   );
 }
