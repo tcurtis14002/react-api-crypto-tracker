de04c8e3f46892f22ee65b84cfd650518317de43
Tyler <willdev0110@gmail.com>
2023-02-12 14:20:56 -0700

    add final commit for logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -12,20 +14,44 @@ function App() {
       )
       .then((response) => {
         setCoins(response.data);
-      })
-      .catch((error) => alert("your app STILL does not work"));
-  });
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
-        <form
-          type="text"
-          placeholder="Search"
-          className="coin-input"
-          action=""></form>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            marketCap={coin.market_cap}
+          />
+        );
+      })}
     </div>
   );
 }
