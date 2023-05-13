0292056f6b5544e25e1064655af4c9f5a9434dd0
Tyler <willdev0110@gmail.com>
2023-05-13 09:46:53 -0600

    remove unused theme var

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
 		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 	const [coins, setCoins] = useState([]);
 	const [search, setSearch] = useState("");
-	const [theme, setTheme] = useState("dark");
 
 	useEffect(() => {
 		axios.get(api).then((response) => {
